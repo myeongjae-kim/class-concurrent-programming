f9b201517d14bd1f0ca81f0e87481ee7904fb309
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2017-09-29 14:32:18 +0900

    iteration number is chagned from 22000 to 11000.

--- project1/include/trie.h
+++ project1/include/trie.h
@@ -5,7 +5,7 @@
 
 // for threads
 #define THREAD_NUM 60
-static const uint32_t SEARCH_ITER_NUM = 22000;
+static const uint32_t SEARCH_ITER_NUM = 11000;
 
 typedef struct _ThreadArg {
   struct Trie* trieRoot; // this value could be a global variable.
