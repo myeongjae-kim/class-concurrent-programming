f010668a29b5cfb3295a938cea0a15ecaa819b2e
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2017-09-29 14:39:17 +0900

    number is changed from 11000 to 17000.

--- project1/include/trie.h
+++ project1/include/trie.h
@@ -5,7 +5,7 @@
 
 // for threads
 #define THREAD_NUM 60
-static const uint32_t SEARCH_ITER_NUM = 11000;
+static const uint32_t SEARCH_ITER_NUM = 17000;
 
 typedef struct _ThreadArg {
   struct Trie* trieRoot; // this value could be a global variable.
